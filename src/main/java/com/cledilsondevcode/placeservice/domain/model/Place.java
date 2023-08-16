e8b53b3e8071a8e71da8f205df3636d387ed2eed
cledilson <cledilsondevoffice@gmail.com>
2023-08-15 23:31:22 -0300

    Iniciando projeto API para gerenciamento de localizações.

---
+++ src/main/java/com/cledilsondevcode/placeservice/domain/model/Place.java
@@ -0,0 +1,85 @@
+package com.cledilsondevcode.placeservice.domain.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    String slug;
+
+    String state;
+
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Place place = (Place) o;
+        return Objects.equals(id, place.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/cledilsondevcode/placeservice/domain/repository/PlaceRepository.java
@@ -0,0 +1,7 @@
+package com.cledilsondevcode.placeservice.domain.repository;
+
+public interface PlaceRepository {
+
+
+
+}
